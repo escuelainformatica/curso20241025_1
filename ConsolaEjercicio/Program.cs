8552a921b95295e65b34878185ac8e4dd72c8a0f
jorgecc <jcastro@eftec.cl>
2024-10-25 16:26:26 -0300

    first

---
+++ ConsolaEjercicio/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsolaEjercicio.modelos;
+using ConsolaEjercicio.servicios;
+
+var alumno1 = new Alumno("john", 4, 4, 4);
+var alumno2 = new Alumno("Susan", 6, 6, 6);
+
+// promedio
+Console.WriteLine(AlumnoServicio.Promedio(alumno1));
+Console.WriteLine(AlumnoServicio.Promedio(alumno2));
+// aprobado
+Console.WriteLine(AlumnoServicio.EstaAprobado(alumno1));
+Console.WriteLine(AlumnoServicio.EstaAprobado(alumno2));
+// comparar
+Console.WriteLine(AlumnoServicio.AlumnoMejorPromedio(alumno1, alumno2));
---
+++ ConsolaEjercicio/servicios/AlumnoServicio.cs
@@ -0,0 +1,42 @@
+﻿using ConsolaEjercicio.modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaEjercicio.servicios
+{
+    internal class AlumnoServicio
+    {
+        public static double Promedio(Alumno alumno)
+        {
+            return (alumno.NotaCastellano + alumno.NotaMatematica 
+                + alumno.NotaHistoria) / 3;
+        }
+        public static string EstaAprobado(Alumno alumno)
+        {
+            if(alumno.NotaHistoria>=4 && alumno.NotaMatematica>=4 &&
+                alumno.NotaCastellano>=4)
+            {
+                return "aprobado";
+            }
+            return "reprobado";
+        }
+        public static string AlumnoMejorPromedio(Alumno alumno1,Alumno alumno2)
+        {
+            var promedio1 = Promedio(alumno1);
+            var promedio2 = Promedio(alumno2);
+            if(promedio1>promedio2)
+            {
+                return alumno1.Nombre;
+            } else if(promedio2 >promedio1 )
+            {
+                return alumno2.Nombre;
+            } else
+            {
+                return "Tienen el mismo promedio";
+            }
+        }
+    }
+}
